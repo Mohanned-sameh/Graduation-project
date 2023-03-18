d4346a453b62cd3d8f49381a583aacb063a78fdf
Mohanned-sameh <mohanned2678@gmail.com>
2023-03-19 01:34:49 +0200

    removed window.location.reload()

--- frontend/src/components/BookForm.jsx
+++ frontend/src/components/BookForm.jsx
@@ -29,9 +29,6 @@ function BookForm({ id }) {
     setFormdata(formData);
     dispatch(reset);
     navigate("/book/details/");
-    if (formData) {
-      document.location.reload();
-    }
   };
   let min = new Date().toISOString().split("T")[0];
 
