cb1e7140a9e6171ab2727c54d7b9b6ef29818ca8
Mohanned-sameh <mohanned2678@gmail.com>
2023-03-18 23:38:54 +0200

    fixed issues with the book details

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const port = process.env.PORT;
 const { errorHandler } = require("./middleware/errorMiddleware.js");
+const path = require("path");
 const connectDB = require("./config/db.js");
 
 connectDB();
@@ -15,16 +16,22 @@ app.use("/api/admins", require("./routes/adminRoutes"));
 app.use("/api/restaurants", require("./routes/restaurantsRoutes"));
 app.use("/api/order", require("./routes/orderRoutes.js"));
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/build")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(__dirname, "../", "frontend", "build", "index.html");
-//   });
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("Website is still on development");
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static(path.join(__dirname, "../frontend/build"), {
+      dotfiles: "allow",
+    })
+  );
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../", "frontend", "build", "index.html")
+    );
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Website is still on development");
+  });
+}
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on PORT ${port}`));
